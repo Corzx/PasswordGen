2cf4f00a88c9665e0ef9a4626b7318d17c34681d
xCorz <corey_239@hotmail.com>
2022-11-28 15:09:48 +1100

    final

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,7 +1,13 @@
-import string
 import random
+import string
 
-s1 = string.ascii_lowercase
-s2 = string.ascii_uppercase
-s3 = string.digits
-s4 = string.punctuation
+passlen = int(input("Enter the length of your password:"))
+
+letters_upper = string.ascii_uppercase
+letters_lower = string.ascii_lowercase
+numbers = string.digits
+exc = "!"
+pw = letters_lower + letters_upper + numbers
+
+password = "Your new password is: " + "".join(random.sample(pw, passlen - 1)) + "!"
+print(password)
